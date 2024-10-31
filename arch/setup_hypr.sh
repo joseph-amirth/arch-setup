38d824985d23fefb6651f5f622a981f005d5862d
joseph-amirth <epicjoe128@gmail.com>
2024-10-31 18:52:14 +0530

    Update setup_hypr.sh

--- arch/setup_hypr.sh
+++ arch/setup_hypr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S hyprland hyprpaper hyprshot waybar rofi-wayland wl-clipboard dunst
+sudo pacman -S hyprland hyprpaper hyprshot hyprlock hypridle
+sudo pacman -S waybar rofi-wayland wl-clipboard dunst
 
 sudo pacman -S xorg mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
 sudo pacman -S libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
